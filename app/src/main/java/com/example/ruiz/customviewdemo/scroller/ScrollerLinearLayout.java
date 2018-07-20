f4e66b58e75d44b071e1c3a9149d2e843ad5cbca
Ruiz <971961844@qq.com>
2018-07-20 10:24:33 +0800

    scroller

--- app/src/main/java/com/example/ruiz/customviewdemo/scroller/ScrollerLinearLayout.java
+++ app/src/main/java/com/example/ruiz/customviewdemo/scroller/ScrollerLinearLayout.java
@@ -5,7 +5,6 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.Scroller;
 
 public class ScrollerLinearLayout extends LinearLayout {
@@ -13,7 +12,7 @@ public class ScrollerLinearLayout extends LinearLayout {
 
     private final String TAG = ScrollerLinearLayout.class.getSimpleName();
 
-    private float mLastX,mLastY;
+    private float mLastX, mLastY;
 
     private Scroller scroller = new Scroller(getContext());
 
@@ -33,12 +32,12 @@ public class ScrollerLinearLayout extends LinearLayout {
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
 //        super.onLayout(changed, l, t, r, b);
-        for (int i=0; i<getChildCount();i++){
-            if (getOrientation()==HORIZONTAL){
+        for (int i = 0; i < getChildCount(); i++) {
+            if (getOrientation() == HORIZONTAL) {
                 int scrollX = getScrollX();
-            }else {
+            } else {
                 int scrollY = getScrollY();
-                getChildAt(i).layout(0,scrollY+i*getMeasuredHeight(),getMeasuredWidth(),scrollY+getMeasuredHeight()*(i+1));
+                getChildAt(i).layout(0, scrollY + i * getMeasuredHeight(), getMeasuredWidth(), scrollY + getMeasuredHeight() * (i + 1));
             }
         }
     }
@@ -46,43 +45,48 @@ public class ScrollerLinearLayout extends LinearLayout {
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        if (ev.getAction()==MotionEvent.ACTION_MOVE){
+        if (ev.getAction() == MotionEvent.ACTION_MOVE) {
             return true;
-        }else if (ev.getAction() == MotionEvent.ACTION_UP){
+        } else if (ev.getAction() == MotionEvent.ACTION_UP) {
             return false;
-        }else {
+        } else {
             return false;
         }
     }
 
     @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
     public boolean onTouchEvent(MotionEvent event) {
         float x = event.getX(),
-            y = event.getY();
+                y = event.getY();
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 mLastX = x;
                 mLastY = y;
-                if (!scroller.isFinished()){
+                if (!scroller.isFinished()) {
                     scroller.abortAnimation();
                 }
                 return true;
             case MotionEvent.ACTION_MOVE:
-                if (getOrientation()==HORIZONTAL) {
+                if (getOrientation() == HORIZONTAL) {
                     scrollBy(-(int) (x - mLastX), 0);
-                }else {
+                } else {
 
-                    scrollBy(0,- (int) (y - mLastY));
+                    scrollBy(0, -(int) (y - mLastY));
                 }
                 mLastX = x;
                 mLastY = y;
                 return true;
             case MotionEvent.ACTION_UP:
-                if (getOrientation() == HORIZONTAL){
-                    smoothScroll((int)(event.getX()-mLastX),0);
-                }else {
-                    smoothScroll(0,(int)Math.ceil(event.getY()-mLastY));
+                if (getOrientation() == HORIZONTAL) {
+                    smoothScroll((int) (event.getX() - mLastX), 0);
+                } else {
+                    smoothScroll(0, (int) Math.ceil(event.getY() - mLastY));
                 }
                 mLastX = x;
                 mLastY = y;
@@ -96,16 +100,16 @@ public class ScrollerLinearLayout extends LinearLayout {
     }
 
 
-    private void smoothScroll(int distX, int distY){
+    private void smoothScroll(int distX, int distY) {
 
         int scrollX = getScrollX();
         int scrollY = getScrollY();
-        if (getOrientation()==HORIZONTAL){
-            int delta = distX-scrollX;
-            scroller.startScroll(scrollX,0,distX,0,1000);
-        }else {
-            int delta =  scrollY - distY;
-            scroller.startScroll(0,scrollY,0,-distY,1000);
+        if (getOrientation() == HORIZONTAL) {
+            int delta = distX - scrollX;
+            scroller.startScroll(scrollX, 0, distX, 0, 1000);
+        } else {
+            int delta = scrollY - distY;
+            scroller.startScroll(0, scrollY, 0, -distY, 1000);
         }
 
     }
@@ -113,8 +117,8 @@ public class ScrollerLinearLayout extends LinearLayout {
     @Override
     public void computeScroll() {
         super.computeScroll();
-        if (scroller.computeScrollOffset()){
-            scrollTo(scroller.getCurrX(),scroller.getCurrY());
+        if (scroller.computeScrollOffset()) {
+            scrollTo(scroller.getCurrX(), scroller.getCurrY());
             postInvalidate();
         }
     }
